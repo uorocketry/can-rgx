2159a0b8e912d52d004e2bd19b19d43580f46a48
Jonathan Papineau <jonapap2001@gmail.com>
2020-04-16 19:32:59 -0400

    Added superclass BaseSensor for code common to all sensors

--- sensors/ThermoList.py
+++ sensors/ThermoList.py
@@ -1,24 +1,28 @@
 from Thermo_1wire import Thermo
+from Sensor import BaseSensor
 import glob
 import os
+import csv
 
-class ThermoList:
-	def __init__(self):
-            base_dir = '/sys/bus/w1/devices/'
-            device_folders = glob.glob(base_dir + '28*')
+
+class ThermoList(BaseSensor):
+    def __init__(self):
+        super().__init__("thermo.csv", ["id", "timestamp", "value"])
         
-	    self.sensor_list = list()
-		
-	    for i in device_folders:
-                t = Thermo(os.path.basename(i))
-		self.sensor_list.append(t)
-			
-	def readAll(self):
-	    result = list()	
-            for i in self.sensor_list:
-                result.append(i.read())
-            return result
+        base_dir = '/sys/bus/w1/devices/'
+        device_folders = glob.glob(base_dir + '28*')
+    
+        self.sensor_list = list()
+        
+        for i in device_folders:        
+            t = Thermo(os.path.basename(i))
+            self.sensor_list.append(t)
+            
+    def readAll(self):
+        result = list() 
+        for i in self.sensor_list:
+            result.append(i.read())
+        return result
 
-t = ThermoList()
-
-print(t.readAll())
+    def loop(self):
+        raise NotImplementedError
