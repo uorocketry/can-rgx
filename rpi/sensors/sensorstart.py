0a35334e476096a9064f2a6d190b6a0da5f3d5d0
aa-sharma <58858681+aa-sharma@users.noreply.github.com>
2020-06-13 10:50:05 -0400

    Update sensorstart.py

--- rpi/sensors/sensorstart.py
+++ rpi/sensors/sensorstart.py
@@ -4,9 +4,12 @@ import time
 from multiprocessing import Process
 
 from rpi.sensors.thermometer import ThermoList
+from rpi.sensors.pressure import Pressure
+
 
 def start_sensors():
-    sensorList = [ThermoList()]
+    sensorList = [ThermoList(), Pressure()]
+
 
     logging.getLogger(__name__).info("Starting sensor logging")
     for i in sensorList:
