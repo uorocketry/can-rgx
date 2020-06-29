8640ea1cd980c015a3dcf23ba46be7fbf9c06812
Jonathan Papineau <jonapap2001@gmail.com>
2020-06-28 22:37:53 -0400

    More code cleanup

--- mainRPI.py
+++ mainRPI.py
@@ -1,15 +1,14 @@
+import logging.handlers
 import sys
-import logging, logging.handlers
-from datetime import datetime
 
-from shared.customlogging.handler import MakeFileHandler
-from shared.customlogging.filter import SensorFilter
-from rpi.sensors.sensorstart import start_sensors
-from rpi.network.server import server_listen_forever
-from rpi.network.bufferedsockethandler import BufferedSocketHandler
 import shared.config as config
+from rpi.network.bufferedsockethandler import BufferedSocketHandler
+from rpi.network.server import server_listen_forever
+from rpi.sensors.sensorstart import start_sensors
+from shared.customlogging.filter import SensorFilter
+from shared.customlogging.handler import MakeFileHandler
 
-#Setting up logging to console and file
+# Setting up logging to console and file
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -17,13 +16,13 @@ logger.setLevel(logging.INFO)
 loggingFormat = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 loggingFilter = SensorFilter()
 
-#Logging to console
+# Logging to console
 consoleHandler = logging.StreamHandler(sys.stdout)
 consoleHandler.setFormatter(loggingFormat)
 consoleHandler.addFilter(loggingFilter)
 logger.addHandler(consoleHandler)
 
-#Logging to file. A new file is created each run, with the name being the current date and time
+# Logging to file. A new file is created each run, with the name being the current date and time
 fileHandler = MakeFileHandler('rpi', 'main')
 fileHandler.setFormatter(loggingFormat)
 fileHandler.addFilter(loggingFilter)
@@ -32,7 +31,7 @@ logger.addHandler(fileHandler)
 RPIConfig = config.get_config('rpi')
 socketHandler = BufferedSocketHandler(RPIConfig['laptop_ip'], logging.handlers.DEFAULT_TCP_LOGGING_PORT)
 logger.addHandler(socketHandler)
- 
+
 start_sensors()
 
 server_listen_forever()
