2f493156045ae37ed9aecb537382f3b69d64882a
aryastark-beep <58858681+aryastark-beep@users.noreply.github.com>
2020-07-04 19:27:43 -0400

    Latest acceleration

--- rpi/sensors/accelerometer.py
+++ rpi/sensors/accelerometer.py
@@ -1,5 +1,8 @@
 import spidev
 import time
+import struct
+
+from rpi.sensors.sensorlogging import SensorLogging
 
 #relevant registers
 DATA_FORMAT =  0x31      #clear bit d6 for 4 wire, set for 3 wire
@@ -20,27 +23,29 @@ SPIBus = 0
 SPIDevice = 1     #0 or 1
 
 # Data Range
-RANGE_2G = 0b00000100  #10bit resolution , +/- 2g
-RANGE_4G = 0b00001101
-RANGE_8G = 0b00001110
-RANGE_16G = 0b00001111
+RANGE_2G = 0b00001100  #full resolution (10bits) , MSB mode, +/- 2g  max in flight is 0-2g
+RANGE_4G = 0b00001101  #full resolution (11bits), MSB mode, +/- 4g 
+RANGE_8G = 0b00001110  #full resolution(12bits), MSB mode, +/-8g
+RANGE_16G = 0b00001111 #full resolution(13bits), MSB mode, +/-16g
+#self test:
+SELF_TEST = 0b10001111   #full resolution (13bits) MSB mode, +/-16g
 
 #Other macros
 READ_BIT = 0x01
 WRITE_BIT = 0x00
 DUMMY_BYTE = 0x00
 MEASURE_MODE = 0b00001000 
-OUTPUT_RATE = 0b00001111
+OUTPUT_RATE = 0b00001111    #value for BW register ; 3200Hz
 
 #Conversion factor
-CONV_FULLR = 3.9
+CONV_FULLR = 3.9    #applicable for all gs
 CONV_2G_10B = 3.9   #mg/LSB
 CONV_4G_10B = 7.8
 CONV_8G_10B = 15.6
 CONV_16G_10B = 31.2
 
-class Accelerometer:
-    def __init__(self, measure_range=RANGE_2G):
+class Accelerometer(SensorLogging):
+    def setup(self, measure_range=RANGE_2G):
         # SPI init
         self.spi = spidev.SpiDev()
         self.spi.open(SPIBus, SPIDevice)
@@ -88,10 +93,10 @@ class Accelerometer:
             cat_add.append(address << 1 | READ_BIT)   #sets LSB to 1
         cat_add.append(DUMMY_BYTE)
 
-        ret = self.spi.xfer2(cat_add)[1:]
-        
-        #this line doesn't seem to be working, data is updated in cat_add
-        read_data = self.spi.readbytes(6)
+        ret_array = []
+        for i in range(len(cat_add)):
+            retval = self.spi.xfer2([cat_add[i], 0x00])[1:]
+            ret_array.append(retval)
 
         return cat_add
 
@@ -99,11 +104,10 @@ class Accelerometer:
     def _set_measure_range(self, measure_range):
         """Sets measure range on device.
         Args:
-            measure_range (int): Measure range to set in ADXL355.
+            measure_range (int): Measure range to set.
         Returns:
             None
         """
-        print ("Set measure range...")
         self.write_data(DATA_FORMAT, measure_range)
 
     def _enable_measure_mode(self):
@@ -141,9 +145,9 @@ class Accelerometer:
 
 
         # Combine data
-        x_data = (x_data[0] >> 4) + (x_data[1] << 4) 
-        y_data = (y_data[0] >> 4) + (y_data[1] << 4) 
-        z_data = (z_data[0] >> 4) + (z_data[1] << 4) 
+        x_data = x_data[0] | (x_data[1] << 8)
+        y_data = y_data[0] | (y_data[1] << 8)
+        z_data = z_data[0] | (z_data[1] << 8)
 
         # Apply 2s comp
         if x_data & 0x80000 == 0x80000:
@@ -157,17 +161,31 @@ class Accelerometer:
 
         ret = [x_data, y_data, z_data]
 
-        #Convert from LSB/mg to g
+        #Convert from LSB/mg to mg
         for i in range(len(ret)):
-            ret[i] = (ret[i]*CONV_2G_10B)/1000
+            ret[i] = (ret[i]*CONV_FULLR)
 
         # Return values
         return (ret)
 
+    def run(self):
+        inErrorState = False
+        self.setup()
+            while True:
+                try:
+                    self.sensorlogger.info([time.time()*1000, self.get_axes()])
+
+                    if inErrorState:
+                        inErrorState = False
+                        self.logger.warning("Error has been cleared for acceleration sensor.")
+                except OSError:
+                    if not inErrorState:
+                        self.logger.exception("Error while reading from acceleration sensor.")
+                        inErrorState = True
 
 
+"""
 def main():
-    adxl = Accelerometer()
     while True:
       val = adxl.get_axes()
       print(val)
@@ -175,3 +193,4 @@ def main():
 
 if __name__ == "__main__": 
     main()
+    """
