1a1008aae16d1635f48a2cccd22f3af66c55a5df
Jonathan Papineau <jonapap2001@gmail.com>
2020-07-26 13:09:40 -0400

    Create the ErrorManager to simplify the error logging

---
+++ shared/customlogging/errormanager.py
@@ -0,0 +1,62 @@
+import logging
+
+
+class ErrorManager:
+    """
+    Class to help simplify the error logging and management
+
+    The main feature of this class is that errors and warning are kept track of and will not be logged again until they
+    are 'resolved'. The main use of this is to avoid spamming the laptop with a lot of the same error messages.
+    """
+
+    def __init__(self, logger_name):
+        """
+        :param logger_name: The name under which the logger should be created
+        """
+        self.errors = set()
+        self.logger = logging.getLogger(logger_name)
+
+    def __check_error(self, error_id):
+        """
+        Checks if the error_id was already, and if not, track it by adding it to the self.errors set
+        :param error_id: The unique id of the error
+        :return: If error_id was already raised
+        """
+        if error_id not in self.errors:
+            self.errors.add(error_id)
+            return False
+
+        return True
+
+    def error(self, message, error_id):
+        """
+        Logs an error. Multiple call to this function or warning() with the same error_id will not log the error again
+        until the error is resolved by calling the function resolve().
+        :param message: A simple message describing the error
+        :param error_id: A unique id to identify this error
+        """
+        if not self.__check_error(error_id):
+            self.logger.error(message, extra={'errorID': error_id})
+
+    def warning(self, message, error_id):
+        """
+        Logs an warning. Multiple call to this function or error() with the same error_id will not log the error again
+        until the error is resolved by calling the function resolve().
+        :param message: A simple message describing the error
+        :param error_id: A unique id to identify this error
+        """
+        if not self.__check_error(error_id):
+            self.logger.warning(message, extra={'errorID': error_id})
+
+    def resolve(self, message, error_id, always_send=True):
+        """
+        Marks an error as resolved. The message is sent as an informational message.
+        :param message: A simple message describing the resolved error
+        :param error_id: A unique id to identify this error
+        :param always_send: If True, the message will be sent even if no error have been raised before
+        """
+        if error_id in self.errors:
+            self.errors.remove(error_id)
+            logging.info(message, extra={'errorID': error_id})
+        elif always_send:
+            logging.info(message, extra={'errorID': error_id})
