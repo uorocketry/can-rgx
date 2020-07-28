46460afa66f2d89ba866ff462548f87866e38822
Jonathan Papineau <jonapap2001@gmail.com>
2020-07-28 18:03:09 -0400

    Add option to ErrorManager to resend an error after a certain amount of time

--- shared/customlogging/errormanager.py
+++ shared/customlogging/errormanager.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import time
 
 
 class ErrorManager:
@@ -7,50 +8,65 @@ class ErrorManager:
     Class to help simplify the error logging and management
 
     The main feature of this class is that errors and warning are kept track of and will not be logged again until they
-    are 'resolved'. The main use of this is to avoid spamming the laptop with a lot of the same error messages.
+    are 'resolved'. The main use of this is to avoid spamming the laptop with a lot of the same error messages. Other
+    features are supported like only sending allow sending an error again after a set amount of time passed.
     """
 
-    def __init__(self, logger_name):
+    def __init__(self, logger_name, debounce_time=0):
         """
         :param logger_name: The name under which the logger should be created
+        :param debounce_time: Specifies the number of seconds to wait before allowing a message with the same
+        error_id to be sent again, even if it has not been resolved. If this is set to 0, this functionality is
+        disabled, i.e. the error must be solved before being allowed to be sent again.
         """
-        self.errors = set()
+
+        # Stores the raised errors. The dictionary keys are the error_id, and the values are the last time the error was
+        # raised
+        self.errors = dict()
+
         self.logger = logging.getLogger(logger_name)
+        self.debounce_time = debounce_time
 
         # Append this number to the error_id in the functions bellow.
         # This way, different error managers can use the same error_id and not have a collision
         self.id_append = str(random.randint(0, 100000))
 
-    def __check_error(self, error_id):
+    def __allow_logging(self, error_id):
         """
-        Checks if the error_id was already, and if not, track it by adding it to the self.errors set
+        Performs some checks to see if we should allow this error to be logged. If needed, it will also add the error_id
+        to the self.errors to keep track of it.
         :param error_id: The unique id of the error
         :return: If error_id was already raised
         """
         if error_id not in self.errors:
-            self.errors.add(error_id)
-            return False
+            self.errors[error_id] = time.time()
+            return True
+        elif self.debounce_time != 0 and (time.time() - self.errors[error_id]) >= self.debounce_time:
+            self.errors[error_id] = time.time()
+            return True
 
-        return True
+        return False
 
     def error(self, message, error_id):
         """
-        Logs an error. Multiple call to this function or warning() with the same error_id will not log the error again
-        until the error is resolved by calling the function resolve().
+        Logs an error. Multiple call to this function or warning() with the same error_id will only log the error again
+        if the debounce_time is not 0. Resolving the error by calling resolve() resets the error state, i.e.
+        calling this function again will resend the error.
         :param message: A simple message describing the error
         :param error_id: A unique id to identify this error
         """
-        if not self.__check_error(error_id):
+        if self.__allow_logging(error_id):
             self.logger.error(message, extra={'errorID': error_id + self.id_append})
 
     def warning(self, message, error_id):
         """
-        Logs an warning. Multiple call to this function or error() with the same error_id will not log the error again
-        until the error is resolved by calling the function resolve().
+        Logs an warning. Multiple call to this function or error() with the same error_id will only log the error
+        again if the debounce_time is not 0. Resolving the error by calling resolve() resets the error state, i.e.
+        calling this function again will resend the error.
         :param message: A simple message describing the error
         :param error_id: A unique id to identify this error
         """
-        if not self.__check_error(error_id):
+        if self.__allow_logging(error_id):
             self.logger.warning(message, extra={'errorID': error_id + self.id_append})
 
     def resolve(self, message, error_id, always_send=True):
@@ -61,7 +77,7 @@ class ErrorManager:
         :param always_send: If True, the message will be sent even if no error have been raised before
         """
         if error_id in self.errors:
-            self.errors.remove(error_id)
+            self.errors.pop(error_id)
             logging.info(message, extra={'errorID': error_id + self.id_append})
         elif always_send:
             logging.info(message, extra={'errorID': error_id + self.id_append})
