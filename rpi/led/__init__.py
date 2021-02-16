d6bf76a669d1a5dd5e84466c2fbbe59961244c6e
Jonathan Papineau <public@jpapineau.ca>
2021-02-16 16:41:06 -0500

    Add TODOs to LEDs

--- rpi/led/__init__.py
+++ rpi/led/__init__.py
@@ -7,7 +7,7 @@ from DMXEnttecPro import Controller
 
 class LEDs:
     def __init__(self):
-        self.dmx = Controller('/dev/ttyUSB0', auto_submit=True)
+        self.dmx = Controller('/dev/ttyUSB0', auto_submit=True)  # TODO: Use the proper identifier for the USB device
 
         # Use a lock to access the DMX Controller. Not clear if this is needed, but better be safe than worry.
         self.lock = threading.Lock()
@@ -17,7 +17,7 @@ class LEDs:
             logger = logging.getLogger(__name__)
             logger.info("Activating LED {}".format(led_number))
             self.dmx.set_channel(led_number, 50)  # TODO: Set the appropriate intensity value
-            time.sleep(5)
+            time.sleep(5)  # TODO: Check if the LEDs turned on using the Teensy
             self.dmx.set_channel(led_number, 0)
             logger.info("LED {} has finished activating".format(led_number))
 
