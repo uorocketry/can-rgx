f0b8b726396499e00a28ce5f8227c7ab438b514a
Jonathan Papineau <public@jpapineau.ca>
2021-02-16 16:37:51 -0500

    Use the ErrorManager in MotorControl

--- rpi/motor/__init__.py
+++ rpi/motor/__init__.py
@@ -4,6 +4,7 @@ import time
 from enum import IntEnum
 
 from rpi.teensy import Teensy
+from shared.customlogging.errormanager import ErrorManager
 
 
 class MotorDirection(IntEnum):
@@ -14,6 +15,7 @@ class MotorDirection(IntEnum):
 class MotorControl:
     def __init__(self):
         self.teensy = Teensy()
+        self.em = ErrorManager(__name__)
 
     def __start_motor(self, motor_number, motor_direction):
         logger = logging.getLogger(__name__)
@@ -32,8 +34,12 @@ class MotorControl:
         logger.info("Motor {} stopped".format(motor_number + 1))
 
         if status & 1 == 1:
-            # TODO: Use the Error Manager
-            logger.error("Motor {} is in an error state! Did the limit switch break?".format(motor_number + 1))
+            self.em.error("Motor {} is in an error state! Did the limit switch break?".format(motor_number + 1),
+                          motor_number)
+        else:
+            self.em.resolve(
+                "Error for motor {} has been cleared. Please still proceed carefully.".format(motor_number + 1),
+                motor_number, False)
 
     def start_motor(self, motor_number, motor_direction):
         threading.Thread(target=self.__start_motor, args=(motor_number, motor_direction)).start()
