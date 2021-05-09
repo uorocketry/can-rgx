99607039a67942a7b26df875f5cf7903c62e0d0b
Jonathan Papineau <public@jpapineau.ca>
2021-05-09 16:33:35 -0400

    Fix acceleration sensor
    Co-authored-by: Aashna <58858681+aryastark-beep@users.noreply.github.com>

--- rpi/sensors/accelerometer.py
+++ rpi/sensors/accelerometer.py
@@ -1,7 +1,16 @@
 import time
+
 import spidev
 
-class Accelerometer():
+
+def twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    if (val & (1 << (bits - 1))) != 0:  # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)  # compute negative value
+    return val
+
+
+class Accelerometer:
     # registers index
     DATA_FORMAT = 0x31  # clear bit d6 for 4 wire, set for 3 wire
     BW_RATE = 0x2C  # date and power mode control
@@ -23,16 +32,16 @@ class Accelerometer():
     CSHIGH = False  # cs pin low at start of transmission
 
     # Data Range
-    RANGE_2G = 0b00001100  # full resolution (10bits) , left-justified MSB mode, +/- 2g
-    RANGE_4G = 0b00001101  # full resolution (11bits), MSB mode, +/- 4g
-    RANGE_8G = 0b00001110  # full resolution(12bits), MSB mode, +/-8g
-    RANGE_16G = 0b00001111  # full resolution(13bits), MSB mode, +/-16g
+    RANGE_2G = 0b00001000  # full resolution (10bits) , left-justified MSB mode, +/- 2g
+    RANGE_4G = 0b00001001  # full resolution (11bits), MSB mode, +/- 4g
+    RANGE_8G = 0b00001010  # full resolution(12bits), MSB mode, +/-8g
+    RANGE_16G = 0b00001011  # full resolution(13bits), MSB mode, +/-16g
     # self test:
     SELF_TEST = 0b10001111  # full resolution (13bits) MSB mode, +/-16g
 
     # register values
     MEASURE_MODE = 0b00001000  # set device to measure mode (D3)
-    BW_OUTPUT_RATE = 0b00001111  # normal operation, 3200Hz output rate, 1600Hz bandwidth
+    BW_OUTPUT_RATE = 0x0B  # normal operation, 3200Hz output rate, 1600Hz bandwidth
     FIFO_MODE = 0b10011111  # stream mode, INT1 triggered, 31bit buffer
     FIFO_BYPASS = 0b00000000
     READ_BIT = 0x01
@@ -47,7 +56,7 @@ class Accelerometer():
     CONV_8G_10B = 15.6
     CONV_16G_10B = 31.2
 
-    def __init__(self, measure_range=RANGE_4G):
+    def __init__(self, measure_range=RANGE_16G):
         self.spi = spidev.SpiDev()
         self.spi.open(self.SPIBus, self.SPIDevice)
         self.spi.max_speed_hz = self.SPI_MAX_CLOCK_HZ
@@ -55,7 +64,7 @@ class Accelerometer():
 
         self.get_device_id()
         self.set_measure_range(measure_range)
-        self.set_fifo()
+        # self.set_fifo()
         self.set_bw_rate()
         self.enable_measure_mode()
 
@@ -63,8 +72,9 @@ class Accelerometer():
         dev_id = self.xfer_read_byte(0x00, 1)
         address_list = [0x00, 0x1D, 0x1E]
         multibyte_test = self.xfer_read_multiple_bytes_sequential(start_address=0x00, n=3)
-        print(f"Device ID : {dev_id}")
-        print(f"mutlibyte read test : {multibyte_test}")
+        # print(f"Device ID : {dev_id}")
+        # print(f"mutlibyte read test : {multibyte_test}")
+        return dev_id
 
     def set_measure_range(self, measure_range):
         """Function to set range and resolution"""
@@ -91,21 +101,11 @@ class Accelerometer():
         retval = self.spi.xfer2([(address | self.READ_BIT << 7), 0x00])[1:]
         return retval
 
-    def xfer_read_multiple_bytes(self, data_address_list):
-        spi_read = []
-        for address in data_address_list:
-            spi_read.append(address | self.READ_BIT << 7)
-        spi_read.append(self.DUMMY_BYTE)
-        retlist = self.spi.xfer2(spi_read)[1:]
-        #retlist = self.spi.readbytes(12)
-        return retlist
-
     def xfer_read_multiple_bytes_sequential(self, start_address, n):
         spi_read = [start_address | (self.SEQUENTIAL_READ_BYTE << 6)]
         for _ in range(n):
             spi_read.append(self.DUMMY_BYTE)
         retlist = self.spi.xfer2(spi_read)[1:]
-        # retlist = self.spi.readbytes(12)
         return retlist
 
     def get_acceleration_data(self):
@@ -113,50 +113,40 @@ class Accelerometer():
         raw_axes = self.xfer_read_multiple_bytes_sequential(start_address=self.DATA_X0, n=6)
         print(raw_axes)
 
-        x_data = raw_axes[0:2]
-        y_data = raw_axes[2:4]
-        z_data = raw_axes[4:6]
-
-
-        #Combine data
-        x_data = (x_data[1] << 8) | x_data[0]
-        x_data >> 4     #right-shift to discard lower 4 bits
-        y_data = (y_data[1] << 8) | y_data[0]
-        y_data >> 4
-        z_data = (z_data[1] << 8) | z_data[0]
-        z_data >> 4
+        # Combine data
+        x_data = (raw_axes[1] << 8) | raw_axes[0]
+        # x_data >> 4     #right-shift to discard lower 4 bits
+        y_data = (raw_axes[3] << 8) | raw_axes[2]
+        # y_data >> 4
+        z_data = (raw_axes[5] << 8) | raw_axes[4]
+        # z_data >> 4
 
         print(f"x: {x_data}")
         print(f"y : {y_data}")
         print(f"z : {z_data}")
 
         # Apply 2s comp
-        if x_data & 0x80000 == 0x80000:
-            x_data = ~x_data + 1
-
-        if y_data & 0x80000 == 0x80000:
-            y_data = ~y_data + 1
-
-        if z_data & 0x80000 == 0x80000:
-            z_data = ~z_data + 1
+        x_data = twos_comp(x_data, 16)
+        y_data = twos_comp(y_data, 16)
+        z_data = twos_comp(z_data, 16)
 
         ret = [x_data, y_data, z_data]
 
-        #Convert from LSB/mg to mg
+        # Convert from LSB/mg to mg
         for i in range(len(ret)):
-            ret[i] = (ret[i]*self.CONV_FULLR)
+            ret[i] = (ret[i] * 0.004 * 9.8)
 
         # Return values
-        print (ret)
+        print(ret)
 
 
 
 
 def main():
+    adxl = Accelerometer()
     while True:
-        adxl = Accelerometer()
         val = adxl.get_acceleration_data()
-        print(val)
+        # print(val)
         time.sleep(1)
 
 
