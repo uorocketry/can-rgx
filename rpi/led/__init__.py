13e24b61422331cdc48f2110ba18e1c18dcb443b
Jonathan Papineau <public@jpapineau.ca>
2021-07-07 22:31:21 -0400

    Check the photodiodes on the RPi

--- rpi/led/__init__.py
+++ rpi/led/__init__.py
@@ -4,6 +4,7 @@ import time
 
 from DMXEnttecPro import Controller
 
+from rpi.teensy import Teensy
 from shared.customlogging.errormanager import ErrorManager
 
 
@@ -19,15 +20,25 @@ class LEDs:
 
         # Use a lock to access the DMX Controller. Not clear if this is needed, but better be safe than worry.
         self.lock = threading.Lock()
+        self.teensy = Teensy()
 
     def __activate_led(self, led_number):
         with self.lock:
             logger = logging.getLogger(__name__)
-            logger.info("Activating LED {}".format(led_number))
+            logger.info("Activating LED {}".format(led_number + 1))
             self.dmx.set_channel(led_number, 50)  # TODO: Set the appropriate intensity value
-            time.sleep(5)  # TODO: Check if the LEDs turned on using the Teensy
+            time.sleep(5)
+
+            # Use the photodiode to verify if the LED turned on
+            led_state = self.teensy.get_led_state()
+            logger.debug("LED State: {}".format(led_state + 1))
+            if (led_state << led_number) & 0b1 == 1:
+                logger.info("Successfully verified LED {} turned on".format(led_number + 1))
+            else:
+                logger.error("LED {} did not turn on".format(led_number + 1))
+
             self.dmx.set_channel(led_number, 0)
-            logger.info("LED {} has finished activating".format(led_number))
+            logger.debug("LED {} has finished activating".format(led_number + 1))
 
     def activate_led(self, led_number):
         threading.Thread(target=self.__activate_led, args=([led_number])).start()
