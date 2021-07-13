b38c0432fd7506e2dcfd44dfadb08f3f305f9918
Jonathan Papineau <public@jpapineau.ca>
2021-07-12 20:04:42 -0400

    Add heater status to GUI

--- laptop/gui/sensorgui.py
+++ laptop/gui/sensorgui.py
@@ -2,6 +2,31 @@ import logging
 import tkinter as tk
 
 
+class HeaterFrame(tk.Frame, logging.Handler):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, highlightthickness=1, highlightbackground="black")
+        logging.Handler.__init__(self)
+
+        self.title = tk.Label(self, text="Heater State", font=("Arial", 18))
+        self.title.grid(row=0, column=0)
+
+        self.value = tk.Label(self, text="INVALID", font=("Arial", 18))
+        self.value.grid(row=1, column=0)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+        logging.getLogger("sensorlog.temp_management").addHandler(self)
+
+    def emit(self, record):
+        heaterOn = record.heaterOn
+
+        if heaterOn:
+            self.value.config(text="Heating")
+        else:
+            self.value.config(text="Off")
+
+
 class ThermometerFrame(tk.Frame, logging.Handler):
     def __init__(self, parent):
         super().__init__(parent, highlightthickness=1, highlightbackground="black")
@@ -45,8 +70,8 @@ class SensorGUI:
         self.thermometer = ThermometerFrame(master)
         self.thermometer.grid(row=0, column=0)
 
-        self.thermometer = VibrationFrame(master)
-        self.thermometer.grid(row=0, column=1)
+        self.heater = HeaterFrame(master)
+        self.heater.grid(row=0, column=1)
 
         master.grid_columnconfigure(0, weight=1)
         master.grid_columnconfigure(1, weight=1)
