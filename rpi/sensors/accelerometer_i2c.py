52b0050be8e3555a135cd177535322dab2aac0c4
Jonathan Papineau <public@jpapineau.ca>
2021-08-07 14:28:56 -0400

    Fix error

--- rpi/sensors/accelerometer_i2c.py
+++ rpi/sensors/accelerometer_i2c.py
@@ -3,11 +3,13 @@ import smbus
 from rpi.sensors.sensorlogging import SensorLogging
 from shared.customlogging.errormanager import ErrorManager
 
+
 def twos_comp(val, bits):
     """compute the 2's complement of int value val"""
     if (val & (1 << (bits - 1))) != 0:  # if sign bit is set e.g., 8bit: 128-255
         val = val - (1 << bits)  # compute negative value
     return val
+
 
 # registers index
 DATA_FORMAT = 0x31  # clear bit d6 for 4 wire, set for 3 wire
@@ -37,7 +39,7 @@ BW_OUTPUT_RATE = 0b00001101  # normal operation, 800Hz output rate (max)
 FIFO_MODE = 0b10000001  # stream mode, INT1 triggered, 31bit buffer
 FIFO_BYPASS = 0b00000000
 
-#I2C specific read/write with ALT ADDRESS pin high
+# I2C specific read/write with ALT ADDRESS pin high
 READ_BIT = 0x3B
 WRITE_BIT = 0x3A
 DUMMY_BYTE = 0x00
@@ -49,16 +51,16 @@ EARTH_GRAVITY = 9.80665
 # Precision of the output, how many decimals to keep
 OUTPUT_PRECISION = 3
 
-#I2C Config
+# I2C Config
 ACC_DEVICE_ID = 0xE5
-ACC_ADDRESS = 0x53  #CORRECT THIS VALUE depending on physical connection (ALT ADDRESS LOW)
+ACC_ADDRESS = 0x53  # CORRECT THIS VALUE depending on physical connection (ALT ADDRESS LOW)
+
 
 class Accelerometer(SensorLogging):
     def setup(self, measure_range=RANGE_16G):
-        self.bus = smbus.SMBus(1)  #correct this too
+        self.bus = smbus.SMBus(1)  # correct this too
 
         self.check_sensor_connection()
-
 
         self.set_measure_range(measure_range)
         self.set_fifo()
@@ -73,13 +75,13 @@ class Accelerometer(SensorLogging):
         """
         em = ErrorManager(__name__)
 
-        if self.bus.read_byte_data(0x00) == ACC_DEVICE_ID:
+        if self.bus.read_byte_data(ACC_ADDRESS, 0x00) == ACC_DEVICE_ID:
             return
 
         em.error("Acceleration sensor is not connected", "ACCEL_CONNECTION")
 
         # Wait until the sensor connects
-        while self.bus.read_byte_data(0x00) == ACC_DEVICE_ID:
+        while self.bus.read_byte_data(ACC_ADDRESS, 0x00) == ACC_DEVICE_ID:
             pass
 
         em.resolve("Acceleration sensor connected", "ACCEL_CONNECTION")
@@ -111,17 +113,17 @@ class Accelerometer(SensorLogging):
         z_data0 = self.bus.read_byte_data(ACC_ADDRESS, DATA_Z0)
         z_data1 = self.bus.read_byte_data(ACC_ADDRESS, DATA_Z1)
 
-        #Combine respective registers
-        x_data = ((x_data1 & 0x03) * 256 ) + x_data0
-        y_data = ((y_data1 & 0x03) * 256 ) + y_data0
-        z_data = ((z_data1 & 0x03) * 256 ) + z_data0
+        # Combine respective registers
+        x_data = ((x_data1 & 0x03) * 256) + x_data0
+        y_data = ((y_data1 & 0x03) * 256) + y_data0
+        z_data = ((z_data1 & 0x03) * 256) + z_data0
 
-        #Apply 2s comp
+        # Apply 2s comp
         x_data = twos_comp(x_data, 16)
         y_data = twos_comp(y_data, 16)
         z_data = twos_comp(z_data, 16)
 
-        #Convert to m/s^2 and round
+        # Convert to m/s^2 and round
         x_data *= CONV_FULLR * EARTH_GRAVITY
         y_data *= CONV_FULLR * EARTH_GRAVITY
         z_data *= CONV_FULLR * EARTH_GRAVITY
@@ -133,13 +135,10 @@ class Accelerometer(SensorLogging):
 
         return {"x": x_data, "y": y_data, "z": z_data}
 
-
     def run(self):
         super().setup_logging("acceleration", ["x", "y", "z"])
         self.setup()
         while True:
             acceleration = self.get_acceleration_data()
+            print(acceleration)
             self.sensorlogger.info([time.time() * 100, acceleration["x"], acceleration["y"], acceleration["z"]])
-
-
-
