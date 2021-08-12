825d3ba12aba7940c25fe5de3e208a425b6f3924
Jonathan Papineau <public@jpapineau.ca>
2021-08-11 21:58:45 -0400

    Add retry when communicating to Teensy
    Fix #49

--- rpi/teensy/__init__.py
+++ rpi/teensy/__init__.py
@@ -5,8 +5,11 @@ avoid possible race conditions.
 import threading
 
 from smbus import SMBus
+from tenacity import retry, stop_after_attempt
 
 TEENSY_ADDRESS = 0x8
+
+RETRY_TIMES = 3
 
 
 # These value are sent to the Teensy to select what it will return on a read operation
@@ -26,11 +29,13 @@ class Teensy:
     def activate_motor(self, motor_number, motor_direction):
         self.bus.write_byte(TEENSY_ADDRESS, 0b100 | ((motor_number & 1) << 1) | motor_direction & 1)
 
+    @retry(stop=stop_after_attempt(3))
     def get_motor_state(self):
         with Teensy.lock:
             self.bus.write_byte(TEENSY_ADDRESS, I2CReadState.MOTOR & 0b11)  # Tell the Teensy to return motor info
             return self.bus.read_byte(TEENSY_ADDRESS)
 
+    @retry(stop=stop_after_attempt(3))
     def get_led_state(self):
         with Teensy.lock:
             self.bus.write_byte(TEENSY_ADDRESS, I2CReadState.LED & 0b11)  # Tell the Teensy to return led info
